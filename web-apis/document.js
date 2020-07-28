d23f5e1748b0bf68bd6975c750726b5f81d8e9af
smarten <smarten.developer@gmail.com>
2020-07-27 19:44:13 -0700

    how to target html elements with js

--- web-apis/document.js
+++ web-apis/document.js
@@ -5,3 +5,8 @@ console.log(document.body.children) // Returns an array of the tags below the bo
 console.log(document.body.childNodes) // Returns an array with all of the children plus their content
 
 console.log(document.body.children.childNodes[0].style.color) // Accesses a style element that we can then modify
+
+document.getElementById() // How to find an element by ID
+document.querySelector() // Also works, more powerful, can find classes and more than 1 level of classes
+
+var $articleDiv = document.getElementById() // Dollar sign signifies the variable is an html element
