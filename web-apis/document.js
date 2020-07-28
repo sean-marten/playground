b18425406e9984579e0cffdf2f2a1d6110763172
smarten <smarten.developer@gmail.com>
2020-07-27 21:30:12 -0700

    hoisting notes

--- web-apis/document.js
+++ web-apis/document.js
@@ -15,3 +15,59 @@ document.querySelectorAll('p').setAttribute("style", "font-size: 65px;"); // Can
 
 document.createElement(tagName); // Will create an HTML element
 document.body.appendChild(tag); // Will add it to the page
+
+// No two arrays and no two objects are ever the same
+
+const trashcan1 = [];                 
+const trashcan2 = [];
+trashcan1 === trashcan2; // This is false (also with ==)
+const trashcan3 = trashcan1;
+trashcan1 === trashcan3; // This is true
+
+const nums = [1,2,3];
+nums.push[4]; // This works, even with a const
+
+// Hoisting in Javascript: JS interprets files not line by line, it hoists first.
+
+var name = 'Sean';
+var lastName = 'Marten';
+
+// Can be called before being declared because of hoisting
+sayHi();
+
+function sayHi() {
+    console.log("say hi");
+}
+
+
+var sayHello = function() {
+    console.log('say hellow');
+}
+
+// This has to be after, since it is is declared like a variable
+sayHello();
+
+// But JS reads it like this...
+// First looks at all variable declarations
+
+// Variable declarations get hoisted, but not their values;
+// Then it reads code line by line
+var name;
+var lastName;
+var sayHello;
+// These are hoisted to the top
+function sayHi() {
+    console.log('say Hi');
+}
+
+name = 'Sean';
+lastName = 'Marten';
+sayHi();
+sayHello = function() {
+    console.log('say hellow');
+}
+
+sayHello();
+
+sayHi();
+// LET AND CONST DO NOT HOIST
