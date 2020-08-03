d188c3f1e3387af5f87ed749b594e924661b22d7
smarten <smarten.developer@gmail.com>
2020-08-02 21:19:32 -0700

    react notes

--- javascript/notes/things-to-lookup.js
+++ javascript/notes/things-to-lookup.js
@@ -1,6 +1,37 @@
-// For each statements
-
 // The four rules of this
     // By defaut, if this is not referencing an object, it returns the window object
+    // The 4 different cases of this?
+        // 4 - this means window
+        // 3 - this means inside object
+        // 2 -
+        // 1 -
 
-// GitGoodAtJavascript (in Manny's repo)
+// Different types architecture and protocols of APIs
+    // REST
+    // FAST
+    // SOAP
+    // XML-RPC
+    // JSON-RPC
+
+// Main types of Web APIs
+    // Open APIs
+    // Partner APIs
+    // Internal APIs
+    // Composite APIs
+        // These are all just who these APIs are available to
+        // For now we will need to use open source APIs
+
+// What is Ajax?
+    // It is a call you use when working with an API (will be useful for weather dashboard)
+    // Where you use the url associated with the API (mostly every web api call)
+    // Seems fairly similar to the 'WebClient' proxy in C#
+    // This is done with Jquery -- $ means Jquery ( a faster and easier way to write javascript )
+
+// What is a proxy?
+    // The intermediary between users and websites they are looking at, a sort of messenger for your request
+    // This 'messenger' will contain authentication other parameters.
+
+// With browser APIs, are we just talking about APIs specific to the browser? Like Chrome, Firefox, etc.
+    // yes ^^
+
+// For project 1 use materialize
