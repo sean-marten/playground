6e2a99f9cb2efe136c158649d6a83f5758641f7c
smarten <smarten.developer@gmail.com>
2020-08-05 21:29:29 -0700

    few changes to notes

--- jquery/syntax.js
+++ jquery/syntax.js
@@ -8,21 +8,23 @@ $(".btn").on("click", function () {
 
 // Event delegation:
 $(".btn").on("click", function () {
-    alert("yeet");
-    const $btn = $("<button>").text("yolo").addClass("btn");
-    $drinks.append($btn);
-  });
+  alert("yeet");
+  const $btn = $("<button>").text("yolo").addClass("btn");
+  $drinks.append($btn);
+});
 
 // The button that gets added, do not work, this is because, our anchor (the yolo button), did not exist when the onclick event handler was rendered, so the event handler essentially does not get "attached" to the yolo button
 
 // To do this properly
-$(document).on('click', '.btn') // This now lets us target all buttons on the document, if they are added now or in the future
+$(document).on("click", ".btn"); // This now lets us target all buttons on the document, if they are added now or in the future
 
 // To get this in jQuery: (in an event listening it would be what you clicked on)
-$(this)
+$(this);
 
 $(".btn").on("click", function () {
-    $(this).text() // returns the text of the button you clicked on
-  });
+  $(this).text(); // returns the text of the button you clicked on
+});
+
+let id = $(this).attr("id");
 
 // You can access anything in the element you got
